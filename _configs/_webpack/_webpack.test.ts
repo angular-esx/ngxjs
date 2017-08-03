039c643b4ab1aae508be02526102da4b5f062c91
Minh Van <minh.van1789@gmail.com>
2017-08-04 01:26:12 +0700

    Created _webpack.test.ts

---
+++ _configs/_webpack/_webpack.test.ts
@@ -0,0 +1,67 @@
+import { DefinePlugin } from 'webpack';
+import * as merge from 'webpack-merge';
+import * as AddAssetHtmlPlugin from 'add-asset-html-webpack-plugin';
+
+import { environment } from '../../packages/infrastructure';
+
+import { BaseWebpackConfig } from './_webpack.base';
+
+
+class TesingWebpackConfig extends BaseWebpackConfig {
+
+  /* ---------------------Configs---------------------*/
+
+  protected _getDevToolConfig (): Object {
+    return {
+      devtool: 'inline-source-map',
+    };
+  }
+
+  /* ---------------------Loaders---------------------*/
+
+  protected _getIndexTemplateLoader (): Object { return undefined; }
+
+  protected _getStyleLoader (): Object {
+    const { INCLUDE_STYLES } = this._getConstants();
+
+    const rules = [
+      {
+        test: /\.scss$/,
+        use: [
+          'to-string-loader',
+          'css-loader?sourceMap',
+          'postcss-loader',
+          'sass-loader?sourceMap',
+        ],
+        include: INCLUDE_STYLES,
+      },
+    ];
+
+    return { rules };
+  }
+
+  /* ---------------------Plugins---------------------*/
+
+  protected _getCopyPlugin (): Object { return undefined; }
+
+  protected _getIndexPagePlugin (): Object { return undefined; }
+
+  /* ---------------------Others---------------------*/
+
+  protected _getConstants (): any {
+    if (!this._CONSTANTS) {
+      Object.assign(super._getConstants().PATHS, {
+        DIST_OUTPUT: this._getAbsolutePath('_dist/tesing'),
+        DIST_FONTS: this._getAbsolutePath('_dist/tesing/fonts'),
+        DIST_IMAGES: this._getAbsolutePath('_dist/tesing/images'),
+        DIST_POLYFILLS: this._getAbsolutePath('_dist/tesing/polyfills'),
+        DIST_VENDORS: this._getAbsolutePath('_dist/tesing/vendors'),
+        DIST_STYLES: this._getAbsolutePath('_dist/tesing/styles'),
+      });
+    }
+
+    return this._CONSTANTS;
+  }
+}
+
+export { TesingWebpackConfig };
