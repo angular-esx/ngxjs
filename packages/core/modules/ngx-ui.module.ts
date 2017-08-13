7f94402011d6b937e735c1d683cb9b2c2e1f0df8
Minh Van <congminh@siliconstraits.com>
2017-08-13 20:43:20 +0700

    Updated index declaration for ngx-core

--- packages/core/modules/ngx-ui.module.ts
+++ packages/core/modules/ngx-ui.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
+
+import { NgxCardModule } from './card';
 import { NgxContainerModule } from './container';
 import { NgxGridModule } from './grid';
 import { MenuModule } from './menu';
 import { NgxOverlayModule } from './overlay';
 import { NgxPortalModule } from './portal';
+import { NgxSidenavModule } from './sidenav';
 import { NgxTypographyModule } from './typography';
 import { NgxViewModule } from './view';
-import { NgxCardModule } from './card';
-import { NgxSidenavModule } from './sidenav';
+
 
 const MODULES = [
+  NgxCardModule,
   NgxContainerModule,
   NgxGridModule,
   MenuModule,
   NgxOverlayModule,
   NgxPortalModule,
+  NgxSidenavModule,
   NgxTypographyModule,
   NgxViewModule,
-  NgxCardModule,
-  NgxSidenavModule,
 ];
 
 @NgModule({
