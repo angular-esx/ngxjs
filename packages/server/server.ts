247731cad5884b7b2a1a6cb52f0d9403fd69e901
Minh Van <congminh@siliconstraits.com>
2017-08-20 18:21:37 +0700

    Init ngx-server package for universal test

---
+++ packages/server/server.ts
@@ -0,0 +1,47 @@
+import { enableProdMode } from '@angular/core';
+
+import * as path from 'path';
+import * as express from 'express';
+import * as compression from 'compression';
+import { ngExpressEngine } from '@ngx-universal/express-engine';
+
+import { NgxServerAppModule } from 'ngx-application';
+
+
+enableProdMode();
+
+const CONTEXT = path.resolve(__dirname, '../../');
+const DIST = '_dist/development';
+
+const server: any = express();
+server.use(compression());
+/**
+ * Set view engine
+ */
+server.engine('html', ngExpressEngine({
+  bootstrap: NgxServerAppModule,
+}));
+server.set('view engine', 'html');
+server.set('views', path.join(CONTEXT, DIST));
+/**
+ * Point static path to `public`
+ */
+server.use('/', (express as any).static(path.join(CONTEXT, DIST), { index: false }));
+/**
+ * Catch all routes and return the `index.html`
+ */
+server.get('*', (req, res) => {
+  res.render(`${path.join(CONTEXT, DIST)}/index.html`, { req, res });
+});
+/**
+ * Port & host settings
+ */
+const PORT = 8000;
+const baseUrl = `http://localhost:${PORT}`;
+server.set('port', PORT);
+/**
+ * Begin listening
+ */
+server.listen(server.get('port'), () => {
+  console.log(`Express server listening on ${baseUrl}`);
+});
